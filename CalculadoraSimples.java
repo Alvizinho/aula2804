74feb6b8b316ccefbf1a09988bdcb042a652d1a0
credendim <130668967+credendim@users.noreply.github.com>
2023-05-04 09:38:09 -0300

    Commit credendio sobre divisão

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -1,68 +1,92 @@
 import java.util.Scanner;
 
 public class CalculadoraSimples{
-
     public static void main(String[] args) {
-        
-        //Declaração de variáveis
+        //declaração de variante
         int n1, n2, total, op;
 
-        //Criação e instância do objeto de entrada
+        //Criação e instancia do objeto de entrada
         Scanner entrada = new Scanner(System.in);
 
         do{
-
-            System.out.println("\n\t\t\t -- Calculadora Simples -- \n");
-
+            System.out.println("\n\t\t\t--CalcuLdora Simples--\n");
             System.out.println("1. Soma");
             System.out.println("2. Subtração");
-            System.out.println("3. Sair");
-
+            System.out.println("3. Multiplicação");
+            System.out.println("4. Divisão");
+            System.out.println("5. Sair");
             System.out.print("Opção: ");
             op = entrada.nextInt();
 
             if(op == 1){
-                System.out.println("\n\t\t\t -- Soma -- \n");
-                //Alves
+                System.out.println("\n\t\t\tSoma\n");
+                //Gabriel Alves
 
                 //Entrada
-                System.out.print("Informe N1: ");
+                System.out.println("Informe: N1 ");
                 n1 = entrada.nextInt();
-                System.out.print("Informe N2: ");
+                System.out.println("Informe: N2 ");
                 n2 = entrada.nextInt();
 
                 //Processamento
                 total = n1 + n2;
 
-                //Saída
-                System.out.printf("%d + %d = %d" , n1, n2, total);
+                //Saida
+                System.out.printf("%d + %d = %d", n1, n2, total);
 
-            } else if(op == 2){
-                System.out.println("\n\t\t\t -- Subtração -- \n");
-                //credendio
-                
+            }else if(op == 2){
+                System.out.println("\n\t\t\tSubtração\n");
+                //Gabriel Credendio
+
                 //Entrada
-                System.out.print("Informe N1: ");
+                System.out.println("Informe N1:");
                 n1 = entrada.nextInt();
-                System.out.print("Informe N2: ");
+                System.out.println("Informe N2:");
                 n2 = entrada.nextInt();
 
                 //Processamento
                 total = n1 - n2;
 
-                //Saída
-                System.out.printf("%d - %d = %d" , n1, n2, total);
-                
-            } else if(op == 3){
-                System.out.println("Forte Abraço");
+                //saida
+                System.out.printf("%d - %d = %d", n1, n2, total);
+
+            }else if(op == 3){
+                System.out.println("\n\t\t\tMultiplicação\n");
+                //Pedro Araujo
+
+                //Entradas
+                System.out.println("Informe N1:");
+                n1 = entrada.nextInt();
+                System.out.println("Informe N2:");
+                n2 = entrada.nextInt();
+
+                //processamento
+                total = n1 * n2;
+
+                //saida
+                System.out.printf("%d * %d = %d", n1, n2, total);
+
+            }else if(op == 4){
+                System.out.println("\n\t\t\tDivisão\n");
+                //Gabriel Credendio
+
+                //Entradas
+                System.out.println("informe N1:");
+                n1 = entrada.nextInt();
+                System.out.println("informe N2:");
+                n2 = entrada.nextInt();
+
+                //Processamento 
+                total = n1 / n2;
+
+                //Saida
+                System.out.printf("%d / %d = %d", n1, n2, total);
+
+            }else if(op == 5){
+                System.out.println("\n\t\t\tTchau!!\n");
             }else{
-                System.out.println("Opção " + op + "Incorreta!");
+                System.out.println("Opção" +op + "incorreta");
             }
-
-        }while(op!=3);
-
-
+        }while(op!=5);
     }
-
-
 }
